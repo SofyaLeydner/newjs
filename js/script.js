b3afa8356449fa3729dcc317ebaa3f3f7c0fed2b
Sofya <whynotfrontend@gmail.com>
2020-06-08 12:39:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const obj={
+    name: 'John',
+    age: 25,
+    isMirried: false
+
+};
+
+console.log(obj.age);
+
+let arr = ['orang.jpg', 'dfdsffe.png', 6, 'dsdw.png', []];
+console.log(arr[3]);
+
+const user = 'Ivan';
+
+alert (`Привет ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
