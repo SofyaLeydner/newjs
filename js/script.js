8b6a47477727d2f052383e92cf400e3ce5b4bd63
Sofya <whynotfrontend@gmail.com>
2020-06-10 17:36:24 +0300

    prompt

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
-const obj={
-    name: 'John',
-    age: 25,
-    isMirried: false
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: 'false'
 };
 
-console.log(obj.age);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-let arr = ['orang.jpg', 'dfdsffe.png', 6, 'dsdw.png', []];
-console.log(arr[3]);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-const user = 'Ivan';
-
-alert (`Привет ${user}`);
-
-let incr = 10,
-    decr = 10;
-
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
+console.log(personalMovieDB);
